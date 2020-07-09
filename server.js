616faa0a8627c66395449abb27bfd9afd217f746
darrylgit <darrylsmithup@gmail.com>
2020-07-09 13:51:15 -0400

    Implement morgan logging

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-var winston = require('winston');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+// var winston = require('winston');
+const morgan = require('morgan');
 
 const bodyParser = require('body-parser');
-const app = express()
-app.use(cors())
-app.use(helmet())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
   console.log(userInput);
   if (userInput) {
-    winston.log('info', 'user input: ' + userInput);
+    // winston.log('info', 'user input: ' + userInput);
     res.status(200).json('success');
   } else {
-    winston.error('This guy is messing with us:' + userInput);
-    res.status(400).json('incorrect submission')
+    // winston.error('This guy is messing with us:' + userInput);
+    res.status(400).json('incorrect submission');
   }
-})
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
